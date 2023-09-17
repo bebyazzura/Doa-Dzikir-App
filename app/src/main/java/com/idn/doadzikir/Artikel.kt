be13d0e1ae8c0af92ea4711731930c96558921f3
bebyazzura <bebyazzura6@gmail.com>
2023-09-17 08:23:19 +0700

    Initial commit

---
+++ app/src/main/java/com/idn/doadzikir/Artikel.kt
@@ -0,0 +1,11 @@
+package com.idn.doadzikir
+
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+data class Artikel(
+    val imageArtikel: Int,
+    val titleArtikel: String,
+    val descArtikel: String
+)
---
+++ app/src/main/java/com/idn/doadzikir/DzikirDoa.kt
@@ -0,0 +1,7 @@
+package com.idn.doadzikir
+
+data class DzikirDoa(
+    var desc: String,
+    var lafaz: String,
+    var terjemah: String
+)
